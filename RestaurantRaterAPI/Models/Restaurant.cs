792e141610601da33290ca513ea7787271add0e6
normabunton <te_nj25@hotmail.com>
2021-01-25 16:08:27 -0600

    initial

---
+++ RestaurantRaterAPI/Models/Restaurant.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantRaterAPI.Models
+{
+    public class Restaurant
+    {
+        //this is  where we define what the table looks like
+        //key attributes
+        [Key]
+        /// this is the primary key
+        public int Id { get; set; }
+        
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        public double Rating { get; set; }
+    }
+}
---
+++ RestaurantRaterAPI/Models/RestaurantDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantRaterAPI.Models   //this class acts in a way to communicate with the store where we store the information in the database, so we query through the 
+                                        //database in this class and make changes
+{
+    public class RestaurantDbContext : DbContext      //DBContext is our Base Class//this is a snapshot that looks at the databse as it is 
+                                                        //when we first interacted with it
+                                                        ////once we make chages and save it then they will be applied ot the database
+    {
+        public RestaurantDbContext() : base("DefaultConnection")
+        {
+            
+        }
+
+        //public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
